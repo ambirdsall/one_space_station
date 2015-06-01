54a5a56a5d6cff1e06355ed4c05b284b756fd36a
ambirdsall <ambirdsall@gmail.com>
2015-06-01 15:09:14 -0700

    Add spaces_corrected to text_versions

--- app/models/text_converter.rb
+++ app/models/text_converter.rb
@@ -4,21 +4,32 @@ class TextConverter
 
   def initialize(text)
     @original_text = text
+    @spaces_corrected = []
   end
 
   def text_versions
     {
       original_text: @original_text,
-      converted_text: converted_text
+      converted_text: converted_text,
+      spaces_corrected: @spaces_corrected
     }
   end
+
   protected
 
   def converted_text
     @converted_text ||= convert_text
   end
 
+  # this solution feels pretty hacky: given more time, I'd love to find a more
+  # elegant solution. Maybe writing a custom enumerator?
   def convert_text
-    @original_text.gsub(INTER_SENTENCE_SPACES_MATCHER, CORRECT_SPACING)
+    matches = 0
+    @original_text.gsub(INTER_SENTENCE_SPACES_MATCHER) do |match|
+      matches += 1
+      @spaces_corrected << matches if match != CORRECT_SPACING
+
+      CORRECT_SPACING
+    end
   end
 end
